eee4194eb0c2ece27baa2d827c68d99f43a77edf
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2023-04-14 22:27:30 +0300

    Updates

---
+++ index.53c6ad0d.js
@@ -0,0 +1,2 @@
+const headerEl=document.querySelector(".header"),btnNavEl=document.querySelector(".mobile-nav-btn");btnNavEl.addEventListener("click",(()=>headerEl.classList.toggle("nav-open")));
+//# sourceMappingURL=index.53c6ad0d.js.map
